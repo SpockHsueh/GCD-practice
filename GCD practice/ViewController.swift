ac120866f382707ab03c865ec088c31cbc4684a1
Spoke <spoke@Spokede-MacBook-Air.local>
2018-09-02 14:25:56 +0800

    Use Two Button to select GCD type

--- GCD practice/ViewController.swift
+++ GCD practice/ViewController.swift
@@ -14,13 +14,29 @@ class ViewController: UIViewController {
     @IBOutlet weak var label2: UILabel!
     @IBOutlet weak var label3: UILabel!
     
+    var name: String?
+    var address: String?
+    var head: String?
+    
     let client = APIClient(configuration: .default)
     
     var semaphore = DispatchSemaphore(value: 1)
+    let dispatchGroup = DispatchGroup()
 
     override func viewDidLoad() {
         super.viewDidLoad()
+    }
+    
+    
+    @IBAction func semaphore(_ sender: Any) {
+        cleanDisplay()
         Semaphore()
+    }
+    
+    
+    @IBAction func group(_ sender: Any) {
+        cleanDisplay()
+        group()
     }
     
     func Semaphore() {
@@ -28,12 +44,12 @@ class ViewController: UIViewController {
         let loadingQueue = DispatchQueue.global()
         
         loadingQueue.async {
-            
             self.semaphore.wait()
             self.client.getNameData { (data, error) in
                 if error == nil {
                     self.label1.text = data!
-                    print(self.label1.text)
+                    print("--------------")
+                    print(data!)
                     self.semaphore.signal()
                 }
             }
@@ -41,18 +57,62 @@ class ViewController: UIViewController {
             self.semaphore.wait()
             self.client.getAddressData { (data, error) in
                 self.label2.text = data!
-                print(self.label2.text)
+                print(data!)
                 self.semaphore.signal()
-                
             }
             
             self.semaphore.wait()
             self.client.getHeadData { (data, error) in
                 self.label3.text = data!
-                print(self.label3.text)
+                print(data!)
+                print("--------------")
+                self.semaphore.signal()
             }
-            
         }
     }
+    
+    func group() {
+        print("--------------")
+        print("Dowloading Data")
+        dispatchGroup.enter()
+        self.client.getNameData { (data, error) in
+            self.name = data!
+            print(data!)
+            self.dispatchGroup.leave()
+        }
+        
+        dispatchGroup.enter()
+        self.client.getAddressData { (data, error) in
+            self.address = data!
+            print(data!)
+            self.dispatchGroup.leave()
+        }
+        
+        dispatchGroup.enter()
+        self.client.getHeadData { (data, error) in
+            self.head = data!
+            print(data!)
+            self.dispatchGroup.leave()
+        }
+        
+        dispatchGroup.notify(queue: .main) {
+            self.display()
+            print("--------------")
+        }
+    }
+    
+    func display() {
+        print("Display Data")
+        self.label1.text = self.name
+        self.label2.text = self.address
+        self.label3.text = self.head
+    }
+    
+    func cleanDisplay() {
+        self.label1.text = "Label1"
+        self.label2.text = "Label2"
+        self.label3.text = "Label3"
+    }
+    
 }
 
