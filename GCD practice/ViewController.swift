319e7712a3d90266ace0284872d25b81b1249a3c
Spoke <spoke@Spokede-MacBook-Air.local>
2018-09-02 11:27:23 +0800

    Implement the Dispatch Group Logic, Final Version

--- GCD practice/ViewController.swift
+++ GCD practice/ViewController.swift
@@ -14,46 +14,52 @@ class ViewController: UIViewController {
     @IBOutlet weak var label2: UILabel!
     @IBOutlet weak var label3: UILabel!
     
-    let client = APIClient(configuration: .default)
+    var name: String?
+    var address: String?
+    var head: String?
     
-    var semaphore = DispatchSemaphore(value: 1)
-
+    let client = APIClient(configuration: .default)
+    let dispatchGroup = DispatchGroup()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        Semaphore()
+        group()
     }
     
-    func Semaphore() {
+    func group() {
         
-        let loadingQueue = DispatchQueue.global()
-
-        loadingQueue.async {
-            
-            self.semaphore.wait()
-            self.client.getNameData { (data, error) in
-                if error == nil {
-                    self.label1.text = data!
-                    print(self.label1.text)
-                    self.semaphore.signal()
-                }
-            }
-            
-            self.semaphore.wait()
-            self.client.getAddressData { (data, error) in
-                self.label2.text = data!
-                print(self.label2.text)
-                self.semaphore.signal()
-
-            }
-            
-            self.semaphore.wait()
-            self.client.getHeadData { (data, error) in
-                self.label3.text = data!
-                print(self.label3.text)
-            }
-            
-            self.semaphore.signal()
+        print("Dowloading Data")
+        dispatchGroup.enter()
+        self.client.getNameData { (data, error) in
+            self.name = data!
+            print(data!)
+            self.dispatchGroup.leave()
         }
+    
+        dispatchGroup.enter()
+        self.client.getAddressData { (data, error) in
+            self.address = data!
+            print(data!)
+            self.dispatchGroup.leave()
+        }
+    
+        dispatchGroup.enter()
+        self.client.getHeadData { (data, error) in
+            self.head = data!
+            print(data!)
+            self.dispatchGroup.leave()
+        }
+    
+        dispatchGroup.notify(queue: .main) {
+           self.display()
+        }
+    }
+    
+    func display() {
+        print("Display Data")
+        self.label1.text = self.name
+        self.label2.text = self.address
+        self.label3.text = self.head
     }
 }
 
